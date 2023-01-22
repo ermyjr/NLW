b4a2c4b00f263a700ca1466c3adceb468d708b1a
Elizeu Junior <77870830+ermyjr@users.noreply.github.com>
2023-01-22 21:10:20 +0000

    js04

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 const form = document.querySelector("#formHabits")
 const nlwSetup = new NLWSetup(form)
-const data ={
-    run:['01-22','01-23','01-24']
+const button = document.querySelector('header button')
+
+button.addEventListener('click', add)
+form.addEventListener('change', save)
+
+function add() {
+    const today = new Date().toLocaleDateString('pt-br').slice(0,-5)
+    const dayExists = nlwSetup.dayExists(today)
+
+    if (dayExists) {
+        alert('Dia Ja incluso')
+        return
+    }
+
+
+    nlwSetup.addDay(today)
+
 }
+
+function save(){
+    localStorage.setItem('NLWsetup@habits',JSON.stringify(nlwSetup.data))
+}
+
+
+const data= JSON.parse(localStorage.getItem("NLWsetup@habits")) || {}
 
 nlwSetup.setData(data)
 nlwSetup.load()
